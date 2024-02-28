1d4b57d7588de93109c9d1bfa664160dd75695cb
nganbuiwx <ngan.bui.wx@renesas.com>
2024-02-28 14:50:13 +0700

    Initial commit

---
+++ ios/Counter.m
@@ -0,0 +1,18 @@
+//
+//  Counter.m
+//  NativeModuleReact
+//
+//  Created by rvc on 28/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventEmitter.h"
+
+@interface RCT_EXTERN_MODULE(Counter,RCTEventEmitter)
+
+RCT_EXTERN_METHOD(increment:(RCTResponseSenderBlock)callback)
+RCT_EXTERN_METHOD(decrement:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+@end
